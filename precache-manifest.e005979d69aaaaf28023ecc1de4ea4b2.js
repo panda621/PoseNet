4018f7be78894a2dd5bb4f7c69b89cc88ad47880
Panda621 <aisonchiu621@gmail.com>
2024-10-11 17:45:58 +0800

    Deploy React app to Github Pages

---
+++ precache-manifest.e005979d69aaaaf28023ecc1de4ea4b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10de6d220939caa857c6fa17272a42f",
+    "url": "/PoseNet/index.html"
+  },
+  {
+    "revision": "c8e4b27dcf65cfd629cf",
+    "url": "/PoseNet/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "9ad096660435e2e94bb1",
+    "url": "/PoseNet/static/js/2.1ffeb580.chunk.js"
+  },
+  {
+    "revision": "056d2873e7c2868f4e8e8cad3dc44ef0",
+    "url": "/PoseNet/static/js/2.1ffeb580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e4b27dcf65cfd629cf",
+    "url": "/PoseNet/static/js/main.c3f44cd9.chunk.js"
+  },
+  {
+    "revision": "ef3c4970149d12b9f52d",
+    "url": "/PoseNet/static/js/runtime-main.8c17c5ff.js"
+  }
+]);
